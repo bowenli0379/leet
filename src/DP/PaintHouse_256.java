cbec4bda8db0bd6905ca8b886c3f6e57ee96a406
libowen <bwli0379@gmail.com>
2019-12-06 11:47:03 -0500

    12/06/2019

---
+++ src/DP/PaintHouse_256.java
@@ -0,0 +1,20 @@
+package DP;
+
+public class PaintHouse_256 {
+    public static int minCost(int[][] costs){
+        int len = costs.length;
+        if (costs != null && len == 0)
+            return 0;
+        int[][] dp = costs;
+        for (int i = 1 ; i < len; i ++){
+            dp[i][0] = Math.min(costs[i][0]+dp[i-1][1], costs[i][0]+dp[i-1][2]);
+            dp[i][1] = Math.min(costs[i][1]+dp[i-1][0], costs[i][1]+dp[i-1][2]);
+            dp[i][2] = Math.min(costs[i][2]+dp[i-1][0], costs[i][2]+dp[i-1][1]);
+        }
+        return Math.min(dp[len-1][0], Math.min(dp[len-1][1],dp[len-1][2]));
+    }
+    public static void main(String[] args){
+        int[][] costs = {{14,2,11},{11,14,5},{14,3,10}};
+        System.out.println(minCost(costs));
+    }
+}
